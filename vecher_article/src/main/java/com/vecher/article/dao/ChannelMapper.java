95e812cd8b9723a8dc88539aee44893fa4568af0
vecher <745418984@qq.com>
2020-02-29 18:52:14 +0800

    base 模块删除LabelDao 替换为LabelMapper

---
+++ vecher_article/src/main/java/com/vecher/article/dao/ChannelMapper.java
@@ -0,0 +1,8 @@
+package com.vecher.article.dao;
+
+import com.vecher.article.pojo.Channel;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ChannelMapper extends Mapper<Channel> {
+
+}
---
+++ vecher_article/src/main/java/com/vecher/article/dao/ColumnMapper.java
@@ -0,0 +1,8 @@
+package com.vecher.article.dao;
+
+import com.vecher.article.pojo.Column;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ColumnMapper extends Mapper<Column> {
+
+}
